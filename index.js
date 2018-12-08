06acb5f7bd1c69755cdbafa65ed820bb28370df6
Emmanium <pradoe.ep@gmail.com>
2018-12-08 11:48:44 -0700

    created successful get and post methods to actions api

--- index.js
+++ index.js
@@ -19,6 +19,56 @@ server.use(
 //routing
 server.use('/api/projects', projectRouter);
 
+server.get('/api/actions', (req, res) => {
+  actions.get()
+    .then(array => {
+      res.json(array)
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "Error with database" })
+    })
+})
+
+server.get('/api/actions/:id', (req, res) => {
+  const { id } = req.params;
+  actions.get(id)
+    .then(action => {
+      if (action) {
+        res.json(action)
+      } else {
+        res
+          .status(404)
+          .json({ message: "Invalid ID" })
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "Error with database" })
+    })
+})
+
+server.post('/api/actions', (req, res) => {
+  const body = req.body;
+  if (body.project_id && body.description && body.notes) {
+    actions.insert(body)
+      .then(action => {
+        res.json(action)
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: "Error with database" })
+      })
+  } else {
+    res
+      .status(400)
+      .json({ message: "Please make sure to include a project ID, description, and notes" })
+  }
+})
+
 //server listen
 server.listen(PORT, (req, res) => {
   console.log(`Server is listening on PORT ${PORT}`)
---
+++ routers/action_router.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+actions = require('../data/helpers/actionModel');
+
+
+module.exports = router;
