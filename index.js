8e9043a83e12c5d614087e980ca93a1ce047497f
Emmanium <pradoe.ep@gmail.com>
2018-12-08 11:39:56 -0700

    moved project routing intoo its own js file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 express = require('express');
 server = express();
 
-projects = require('./data/helpers/projectModel');
+projectRouter = require('./routers/project_router');
 actions = require('./data/helpers/actionModel');
 
 helmet = require('helmet');
@@ -17,100 +17,7 @@ server.use(
 );
 
 //routing
-server.get('/api/projects', (req, res) => {
-  projects.get()
-    .then(array => {
-      res.json(array)
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: "Error with database" })
-    })
-})
-
-server.get('/api/projects/:id', (req, res) => {
-  const { id } = req.params;
-  projects.get(id)
-    .then(project => {
-      if (project) {
-        res.json(project)
-      } else {
-        res
-          .status(404)
-          .json({ message: "Invalid ID" })
-      }
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: "Error with database" })
-    })
-})
-
-server.post('/api/projects', (req, res) => {
-  const body = req.body;
-  if (body.name && body.description) {
-    projects.insert(body)
-      .then(project => {
-        res.json(project)
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: "Error with database" })
-      })
-  } else {
-    res
-      .status(400)
-      .json({ message: "Please include a name and description" })
-  }
-})
-
-server.delete('/api/projects/:id', (req, res) => {
-  const { id } = req.params;
-  projects.remove(id)
-    .then(count => {
-      if (count) {
-        res.json(count)
-      } else {
-        res
-          .status(404)
-          .json({ message: "Invalid ID" })
-      }
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: "Error with database" })
-    })
-})
-
-server.put('/api/projects/:id', (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  if (body.name && body.description) {
-    projects.update(id, body)
-      .then(project => {
-        if (project) {
-          res.json(project)
-        } else {
-          res
-            .status(404)
-            .json({ message: "Invalid ID" })
-        }
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: "Error with database" })
-      })
-  } else {
-    res
-      .status(400)
-      .json({ message: "Please include a name and description" })
-  }
-})
+server.use('/api/projects', projectRouter);
 
 //server listen
 server.listen(PORT, (req, res) => {
---
+++ routers/project_router.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const router = express.Router();
+
+projects = require('../data/helpers/projectModel');
+
+router.get('/', (req, res) => {
+  projects.get()
+    .then(array => {
+      res.json(array)
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "Error with database" })
+    })
+})
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  projects.get(id)
+    .then(project => {
+      if (project) {
+        res.json(project)
+      } else {
+        res
+          .status(404)
+          .json({ message: "Invalid ID" })
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "Error with database" })
+    })
+})
+
+router.post('/', (req, res) => {
+  const body = req.body;
+  if (body.name && body.description) {
+    projects.insert(body)
+      .then(project => {
+        res.json(project)
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: "Error with database" })
+      })
+  } else {
+    res
+      .status(400)
+      .json({ message: "Please include a name and description" })
+  }
+})
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  projects.remove(id)
+    .then(count => {
+      if (count) {
+        res.json(count)
+      } else {
+        res
+          .status(404)
+          .json({ message: "Invalid ID" })
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "Error with database" })
+    })
+})
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  if (body.name && body.description) {
+    projects.update(id, body)
+      .then(project => {
+        if (project) {
+          res.json(project)
+        } else {
+          res
+            .status(404)
+            .json({ message: "Invalid ID" })
+        }
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: "Error with database" })
+      })
+  } else {
+    res
+      .status(400)
+      .json({ message: "Please include a name and description" })
+  }
+})
+
+module.exports = router;
